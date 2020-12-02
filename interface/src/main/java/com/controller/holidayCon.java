c14284e407856d2dd212cb9f2d303b28e3163640
kaiser61 <314198635@qq.com>
2020-12-02 15:33:11 +0800

    holiday

--- interface/src/main/java/com/controller/holidayCon.java
+++ interface/src/main/java/com/controller/holidayCon.java
@@ -1,4 +1,79 @@
 package com.controller;
 
+import com.interfaces.holidayDao;
+import com.interfaces.newsDao;
+import com.object.holiday;
+import com.object.news;
+import com.result.Result;
+import com.utils.myUtils;
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+
+@Controller
 public class holidayCon {
+    private Result result=new Result();
+    private String objectName="节日";
+    @RequestMapping("/selectHolidays")
+    @ResponseBody
+    public Result selectHolidays(String type, Integer length){
+        SqlSession session=new myUtils().getSession();
+        holidayDao dao=session.getMapper(holidayDao.class);
+        try{
+            if (type==null){
+                List<holiday> holidays = dao.selectAllHoliday();
+                result.setSelects(holidays,objectName);
+            }else if(type.equals("r")&& length!=null){
+                List<holiday> holidays = dao.selectHoliday_R(length);
+                result.setSelects(holidays,objectName);
+            }else{
+                result.setStatus(400);
+                result.setData("链接参数错误");
+            }
+        }catch(Exception e){
+            result.setExcept("selectHolidays");
+        }
+        session.close();
+        return result;
+    }
+
+    @ResponseBody
+    @RequestMapping(value="/insertHoliday",
+            method={RequestMethod.POST,RequestMethod.GET})
+    public Result insertHoliday(holiday holiday){
+        SqlSession session=new myUtils().getSession_Auto();
+        holidayDao dao=session.getMapper(holidayDao.class);
+        try{
+            Integer integer = dao.insertHoliday(holiday);
+            result.setInsert(integer,objectName,holiday);
+        }catch(Exception e){
+            result.setExcept("insertHoliday");
+        }
+        session.close();
+        return result;
+    }
+
+    @ResponseBody
+    @RequestMapping(value="/deleteHoliday",
+            method={RequestMethod.POST,RequestMethod.GET})
+    public Result deleteHoliday(Integer holidayId){
+        SqlSession session=new myUtils().getSession_Auto();
+        holidayDao dao=session.getMapper(holidayDao.class);
+        try {
+            Integer integer = dao.deleteHoliday(holidayId);
+            result.setDelete(integer,objectName,String.valueOf(holidayId));
+        }catch(Exception e){
+            result.setExcept("deleteHoliday");
+        }
+        session.close();
+        return result;
+    }
+
+
+
+
 }
--- interface/src/main/java/com/interfaces/holidayDao.java
+++ interface/src/main/java/com/interfaces/holidayDao.java
@@ -1,4 +1,29 @@
 package com.interfaces;
 
+import com.object.holiday;
+import org.apache.ibatis.annotations.*;
+
+import java.util.List;
+
+@Mapper
 public interface holidayDao {
+
+    @Select("select * from holiday;")
+    List<holiday> selectAllHoliday();
+
+    @Select("select * from holiday order by rand() limit 0,#{length}")
+    List<holiday> selectHoliday_R(
+            @Param("length") Integer length
+    );
+
+    @Insert("insert into holiday values(#{holidayName}," +
+            "#{holidayDate},#{holidayDate},#{holidayReason});")
+    Integer insertHoliday(holiday holiday);
+
+    @Delete("delete from holiday where holidayId=#{holidayId}")
+    Integer deleteHoliday(
+            @Param("holidayId") Integer holidayId
+    );
+
+    Integer updateHoliday(holiday holiday);
 }
