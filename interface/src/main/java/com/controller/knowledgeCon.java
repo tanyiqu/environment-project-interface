054f0056b9b00ec0832d1db3074a9f84938c13b4
kaiser61 <314198635@qq.com>
2020-12-02 16:09:00 +0800

    knowledge

--- interface/src/main/java/com/controller/knowledgeCon.java
+++ interface/src/main/java/com/controller/knowledgeCon.java
@@ -1,4 +1,74 @@
 package com.controller;
 
+import com.interfaces.holidayDao;
+import com.interfaces.knowledgeDao;
+import com.object.holiday;
+import com.object.knowledge;
+import com.result.Result;
+import com.utils.myUtils;
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+
 public class knowledgeCon {
+    private Result result=new Result();
+    private String objectName="知识";
+    @RequestMapping("/selectKnowledges")
+    @ResponseBody
+    public Result selectKnowledges(String type, Integer length){
+        SqlSession session=new myUtils().getSession();
+        knowledgeDao dao=session.getMapper(knowledgeDao.class);
+        try{
+            if (type==null){
+                List<knowledge> knowledges = dao.selectAllKnowledge();
+                result.setSelects(knowledges,objectName);
+            }else if(type.equals("r")&& length!=null){
+                List<knowledge> knowledges = dao.selectAllKnowledge_R(length);
+                result.setSelects(knowledges,objectName);
+            }else{
+                result.setStatus(400);
+                result.setData("链接参数错误");
+            }
+        }catch(Exception e){
+            result.setExcept("selectKnowledges");
+        }
+        session.close();
+        return result;
+    }
+
+    @ResponseBody
+    @RequestMapping(value="/insertKnowledge",
+            method={RequestMethod.POST,RequestMethod.GET})
+    public Result insertKnowledge(knowledge knowledge){
+        SqlSession session=new myUtils().getSession_Auto();
+        knowledgeDao dao=session.getMapper(knowledgeDao.class);
+        try{
+            Integer integer = dao.insertKnowledge(knowledge);
+            result.setInsert(integer,objectName,knowledge);
+        }catch(Exception e){
+            result.setExcept("insertKnowledge");
+        }
+        session.close();
+        return result;
+    }
+
+    @ResponseBody
+    @RequestMapping(value="/deleteKnowledge",
+            method={RequestMethod.POST,RequestMethod.GET})
+    public Result deleteKnowledge(Integer knowledgeId){
+        SqlSession session=new myUtils().getSession_Auto();
+        knowledgeDao dao=session.getMapper(knowledgeDao.class);
+        try{
+            Integer integer = dao.deleteKnowledge(knowledgeId);
+            result.setInsert(integer,objectName,knowledgeId);
+        }catch(Exception e){
+            result.setExcept("deleteKnowledge");
+        }
+        session.close();
+        return result;
+    }
+    
 }
--- interface/src/main/java/com/interfaces/knowledgeDao.java
+++ interface/src/main/java/com/interfaces/knowledgeDao.java
@@ -1,5 +1,26 @@
 package com.interfaces;
 
-public interface knowledgeDao {
+import com.object.knowledge;
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
 
+import java.util.List;
+
+@Mapper
+public interface knowledgeDao {
+    @Select("select * from knowledge;")
+    List<knowledge> selectAllKnowledge();
+
+    @Select("select * from knowledge order by rand() limit 0,#{length};")
+    List<knowledge> selectAllKnowledge_R(Integer integer);
+
+    @Insert("insert into knoledge values(#{knowledgeName},#{knowledgeUrl});")
+    Integer insertKnowledge(knowledge knowledge);
+
+    @Delete("delete from knowledge where knowledgeId=#{knowledgeId}")
+    Integer deleteKnowledge(Integer knowledgeId);
+
+    Integer updateKnowledge(knowledge knowledge);
 }
